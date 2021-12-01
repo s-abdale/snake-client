367e513e9ca20d20bbbe5e23ec5106f3c67f6360
Safia Abdale <safia.abdale@gmail.com>
2021-12-01 19:39:44 +0000

    Added temporary tests to move the snake

--- client.js
+++ client.js
@@ -14,14 +14,60 @@ const connect = function () {
     console.log(message);
   });
 
-  // connect event:
+  // connect event object:
   conn.on('connect',function(){
     console.log("Successfully connected to game server");
+
     // push name to server:
-    conn.write(`Name: SAA`)
+    conn.write(`Name: SAA`);
+
+    /*
+    // using setInterval - moves snake up indefinitely
+    setInterval(() => {
+      conn.write(`Move: up`)
+    }, 50);
+    
+    // using setTimeout - moves snake up incrementally
+    // move snake up:
+    setTimeout(() => {
+      conn.write(`Move: up`)
+    }, 250);
+
+    // move snake left:
+    setTimeout(() => {
+      conn.write(`Move: left`)
+    }, 300);
+
+    // move snake up again:
+    setTimeout(() => {
+      conn.write(`Move: up`)
+    }, 350);
+
+    // move snake left again:
+    setTimeout(() => {
+      conn.write(`Move: left`)
+    }, 400);
+    
+    // move snake up again:
+    setTimeout(() => {
+      conn.write(`Move: up`)
+    }, 450);
+    */
+
+
+
   });
-  
+
   return conn;
 };
 
 module.exports = connect;
+
+
+// Note on game commands:
+/*
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+*/
