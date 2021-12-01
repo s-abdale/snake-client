b53844b8220fcd8c8dbc2bd98505f36b2abedfb7
Safia Abdale <safia.abdale@gmail.com>
2021-12-01 18:59:01 +0000

    Created separate file for client code, and exported client code using ES6 Shorthand to play code

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',// IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on('data',function(message){
+    console.log(message);
+  });
+
+  return conn;
+
+};
+
+module.exports = connect;
