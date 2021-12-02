3e0e42c0183325544a75c39229b7b85910750116
Safia Abdale <safia.abdale@gmail.com>
2021-12-02 00:44:50 +0000

    Made code interconnected and referential  using ES6 notation, wrote code to move snake on the server using WASD keys

--- client.js
+++ client.js
@@ -12,62 +12,17 @@ const connect = function () {
   // data event:
   conn.on('data',function(message){
     console.log(message);
+
   });
 
-  // connect event object:
+  // connect event:
   conn.on('connect',function(){
     console.log("Successfully connected to game server");
-
     // push name to server:
     conn.write(`Name: SAA`);
-
-    /*
-    // using setInterval - moves snake up indefinitely
-    setInterval(() => {
-      conn.write(`Move: up`)
-    }, 50);
-    
-    // using setTimeout - moves snake up incrementally
-    // move snake up:
-    setTimeout(() => {
-      conn.write(`Move: up`)
-    }, 250);
-
-    // move snake left:
-    setTimeout(() => {
-      conn.write(`Move: left`)
-    }, 300);
-
-    // move snake up again:
-    setTimeout(() => {
-      conn.write(`Move: up`)
-    }, 350);
-
-    // move snake left again:
-    setTimeout(() => {
-      conn.write(`Move: left`)
-    }, 400);
-    
-    // move snake up again:
-    setTimeout(() => {
-      conn.write(`Move: up`)
-    }, 450);
-    */
-
-
-
   });
 
   return conn;
 };
 
 module.exports = connect;
-
-
-// Note on game commands:
-/*
-"Move: up" - move up one square (unless facing down)
-"Move: down" - move down one square (unless facing up)
-"Move: left" - move left one square (unless facing right)
-"Move: right" - move left one square (unless facing left)
-*/
--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+// const connect = require("./client");
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,11 +13,27 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
+  // let delay = 0;
+
   if (key === '\u0003') {
     // \u0003 = ctrl+c. if user inputs ctrl+c, exit the game
+    console.log(`GOODBYE`)
     process.exit();
-  }
+  } else if (key === "w") {
+    connection.write(`Move: up`);
+    console.log(`Moved up`);
+  } else if (key === "a") {
+    connection.write(`Move: left`);
+    console.log(`Moved left`);
+  } else if (key === "s") {
+    connection.write(`Move: down`);
+    console.log(`Moved down`);
+  } else if (key === "d") {
+    connection.write(`Move: right`);
+    console.log(`Moved right`);
+  } else {
+    console.log(`ERROR: incorrect input, try again!`)
+  };
 };
-
 
 module.exports = setupInput;
